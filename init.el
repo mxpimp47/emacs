e8bec27b858de1a6d3d64abc093803bda7c7c903
clayton miller <pimpc@claytonmiller.com>
2014-07-15 19:25:02 -0500

    cleaned up white spaces

--- init.el
+++ init.el
@@ -31,7 +31,7 @@
       mouse-yank-at-point t
       save-place-file (concat user-emacs-directory "places")
       backup-directory-alist `(("." . ,(concat user-emacs-directory
-                 "backups"))))
+                                               "backups"))))
 (load-theme 'tango-dark)
 (require 'package)
 (add-to-list 'package-archives
@@ -48,53 +48,53 @@
 ;; Add extra package repositories
 (require 'package)
 (add-to-list 'package-archives
-'("melpa" . "http://melpa.milkbox.net/packages/")
-'("marmalade" . "http://marmalade-repo.org/packages/"))
+             '("melpa" . "http://melpa.milkbox.net/packages/")
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 ;; List packages we want to always have
 (package-initialize)
 (setq adamrt-packages
-'(ack-and-a-half
-exec-path-from-shell
-flymake-cursor
-flycheck
-js2-mode
-magit
-smex
-web-mode))
+      '(ack-and-a-half
+        exec-path-from-shell
+        flymake-cursor
+        flycheck
+        js2-mode
+        magit
+        smex
+        web-mode))
 
 
 ;; Install the packages above if not installed already
 (when (not package-archive-contents)
-(package-refresh-contents))
+  (package-refresh-contents))
 
 (dolist (pkg adamrt-packages)
-(when (and (not (package-installed-p pkg))
-(assoc pkg package-archive-contents))
-(package-install pkg)))
+  (when (and (not (package-installed-p pkg))
+             (assoc pkg package-archive-contents))
+    (package-install pkg)))
 
 
 ;; A few settings
 (setq user-full-name "Clayton Miller" ;; name for various places
-column-number-mode t ;; show column number on modeline (bar at bottom)
-default-directory "~/" ;; use home directory instead of /usr/lib/Cellar/..
-inhibit-startup-message t ;; don't show emacs startup message
-save-interprogram-paste-before-kill t ;; ask me about this
-scroll-step 1 ;; scroll by one
-truncate-lines t ;; don't wrap lines
-truncate-partial-width-windows nil ;; dont wrap lines
-)
+      column-number-mode t ;; show column number on modeline (bar at bottom)
+      default-directory "~/" ;; use home directory instead of /usr/lib/Cellar/..
+      inhibit-startup-message t ;; don't show emacs startup message
+      save-interprogram-paste-before-kill t ;; ask me about this
+      scroll-step 1 ;; scroll by one
+      truncate-lines t ;; don't wrap lines
+      truncate-partial-width-windows nil ;; dont wrap lines
+      )
 
 ;; Are we on a mac?
 (when (equal system-type 'darwin)
-(exec-path-from-shell-initialize)
-(setq mac-option-modifier 'super
-mac-command-modifier 'meta
-ns-function-modifier 'hyper
-ns-pop-up-frames nil ;; single frame
-ns-use-srgb-colorspace t
-trash-directory "~/.Trash/emacs"
-delete-by-moving-to-trash t))
+  (exec-path-from-shell-initialize)
+  (setq mac-option-modifier 'super
+        mac-command-modifier 'meta
+        ns-function-modifier 'hyper
+        ns-pop-up-frames nil ;; single frame
+        ns-use-srgb-colorspace t
+        trash-directory "~/.Trash/emacs"
+        delete-by-moving-to-trash t))
 
 
 ;; delete trailing whitespace on save
@@ -111,39 +111,39 @@ delete-by-moving-to-trash t))
 (add-hook 'python-mode-hook #'(lambda () (flycheck-mode)))
 
 (defun cleanup-buffer-or-region ()
-"Untabify, indent and delete trailing whitespace from buffer or
+  "Untabify, indent and delete trailing whitespace from buffer or
 region."
-(interactive)
-(save-excursion
-(unless (region-active-p)
-(mark-whole-buffer))
-(untabify (region-beginning) (region-end))
-(indent-region (region-beginning) (region-end))
-(save-restriction
-(narrow-to-region (region-beginning) (region-end))
-(delete-trailing-whitespace))))
+  (interactive)
+  (save-excursion
+    (unless (region-active-p)
+      (mark-whole-buffer))
+    (untabify (region-beginning) (region-end))
+    (indent-region (region-beginning) (region-end))
+    (save-restriction
+      (narrow-to-region (region-beginning) (region-end))
+      (delete-trailing-whitespace))))
 
 (defun recentf-ido-find-file ()
-"Find a recent file using Ido."
-(interactive)
-(let ((file (ido-completing-read "Recent file: " recentf-list nil t)))
-(when file
-(find-file file))))
+  "Find a recent file using Ido."
+  (interactive)
+  (let ((file (ido-completing-read "Recent file: " recentf-list nil t)))
+    (when file
+      (find-file file))))
 
 ;; This is from misc.el
 (defun forward-to-word (arg)
-"Move forward until encountering the beginning of a word. With
+  "Move forward until encountering the beginning of a word. With
 argument, do this that many times."
-(interactive "p")
-(or (re-search-forward (if (> arg 0) "\\W\\b" "\\b\\W") nil t arg)
-(goto-char (if (> arg 0) (point-max) (point-min)))))
+  (interactive "p")
+  (or (re-search-forward (if (> arg 0) "\\W\\b" "\\b\\W") nil t arg)
+      (goto-char (if (> arg 0) (point-max) (point-min)))))
 
 ;; pasteboard issue
 (defun ns-get-pasteboard ()
-"Returns the value of the pasteboard, or nil for unsupported formats."
-(condition-case nil
-(ns-get-selection-internal 'CLIPBOARD)
-(quit nil)))
+  "Returns the value of the pasteboard, or nil for unsupported formats."
+  (condition-case nil
+      (ns-get-selection-internal 'CLIPBOARD)
+    (quit nil)))
 
 ;; Key bindings
 (global-set-key (kbd "C-w") 'backward-kill-word)
